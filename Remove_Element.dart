eced7650e826813e52c48edd47de0abc037e43ea
Anas-114 <anasmpofficial1@gmail.com>
2025-06-13 09:31:02 +0530

    first commit

--- Remove_Element.dart
+++ Remove_Element.dart
@@ -1,1 +1,15 @@
  
+int removeElement(List<int> nums, int val) {
+  var c = [];
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      c.add(nums[i]);
+    }
+  }
+  print(c);
+  return c.length;
+}
+
+void main() {
+  print(removeElement([3, 2, 2, 3], 3));
+}
